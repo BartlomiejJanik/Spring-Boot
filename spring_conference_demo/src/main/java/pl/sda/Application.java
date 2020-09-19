9afe5d0cdbfd47c8b113e88061510d2bd0a7e391
Bartlomiej Janik <yanekk1990@gmail.com>
2020-09-19 12:26:23 +0200

    wstrzykiwanie za pomoca set

--- spring_conference_demo/src/main/java/pl/sda/Application.java
+++ spring_conference_demo/src/main/java/pl/sda/Application.java
@@ -1,4 +1,5 @@
 package pl.sda;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import pl.sda.service.SpeakerService;
@@ -8,10 +9,12 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        SpeakerService speakerService = new SpeakerServiceImpl();
+        SpeakerService speakerService = appContext.getBean("speakerService", SpeakerServiceImpl.class);
 
-        System.out.println(appContext.getBean("speakerRepositiory",SpeakerService.class));
+        // System.out.println(appContext.getBean("speakerRepositiory",SpeakerService.class));
 
         System.out.println(speakerService.findAll().get(0).getFirstName());
+        System.out.println(speakerService.findAll().get(0).getLastName());
+
     }
 }
